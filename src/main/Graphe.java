3a61a48a6576a3c6b73e2ef513445db6f39cd075
clementserrano <clementserrano3@gmail.com>
2018-03-07 17:54:19 +0100

    CS - Représentation graphique

--- src/main/Graphe.java
+++ src/main/Graphe.java
@@ -1,5 +1,6 @@
 package main;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -9,6 +10,8 @@ import static java.util.stream.Collectors.joining;
 public class Graphe {
 
     private Map<Sommet, List<Arc>> adjacence;
+    private Sommet sommetDepart;
+    private int capacite;
 
     public Graphe(String filename){
         try {
@@ -35,6 +38,9 @@ public class Graphe {
                             Integer.valueOf(data[3]));
 
                     adjacence.put(sommet, new ArrayList<>());
+                    if(sommetDepart == null){
+                        sommetDepart = sommet;
+                    }
                 }
             }
 
@@ -49,9 +55,10 @@ public class Graphe {
                     double distance = Outils.distance(listeSommet.get(i),listeSommet.get(j));
 
                     if(distance < 25){
-                        Arc arc = new Arc(sommetA, sommetB, distance);
-                        adjacence.get(sommetA).add(arc);
-                        adjacence.get(sommetB).add(arc);
+                        Arc arc1 = new Arc(sommetA, sommetB, distance);
+                        adjacence.get(sommetA).add(arc1);
+                        Arc arc2 = new Arc(sommetB, sommetA, distance);
+                        adjacence.get(sommetB).add(arc2);
                     }
                 }
             }
@@ -59,6 +66,39 @@ public class Graphe {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public Sommet getSommetDepart() {
+        return sommetDepart;
+    }
+
+    public Map<Sommet, List<Arc>> getAdjacence() {
+        return adjacence;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public void dessine() {
+        JFrame fenetre = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        for(Sommet sommet : adjacence.keySet()){
+            JLabel label = new JLabel(sommet.toString());
+            label.setLocation(sommet.getX()*5, sommet.getY()*5);
+            label.setSize(20, 20);
+            panel.add(label);
+        }
+
+        fenetre.add(panel);
+        fenetre.setSize(1000,1000);
+        fenetre.setVisible(true);
     }
 
     @Override
--- src/main/Tabou.java
+++ src/main/Tabou.java
@@ -1,9 +1,50 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.joining;
+
 public class Tabou {
 
     public String findSolution(Graphe graphe) {
+        // Création d'une solution X0
+        List<Sommet> solution = new ArrayList<>();
+        Set<Sommet> sommetsParcourus = new HashSet<>();
 
-        return "";
+        solution.add(graphe.getSommetDepart());
+        sommetsParcourus.add(graphe.getSommetDepart());
+        Sommet sommetSuivant = graphe.getSommetDepart();
+
+        while(sommetsParcourus.size() != graphe.getAdjacence().keySet().size()){
+            int sommeQuantite = 0;
+            while(sommeQuantite < graphe.getCapacite()){
+                sommetSuivant = getVoisinProche(graphe.getAdjacence().get(sommetSuivant), sommetsParcourus);
+                if(sommeQuantite + sommetSuivant.getQuantite() < graphe.getCapacite()){
+                    sommeQuantite += sommetSuivant.getQuantite();
+                    solution.add(sommetSuivant);
+                }
+                System.out.println(sommetSuivant.getIndex());
+            }
+            solution.add(graphe.getSommetDepart());
+        }
+
+        return solution.stream().map(sommet -> sommet.toString()).collect(joining(""));
+    }
+
+    private Sommet getVoisinProche(List<Arc> arcs, Set<Sommet> sommetsParcourus){
+        double distanceMin = arcs.get(0).getDistance();
+        Sommet resultat = arcs.get(0).getSommetB();
+
+        for (Arc arc : arcs){
+            if(!sommetsParcourus.contains(arc.getSommetB()) && arc.getDistance() < distanceMin){
+                distanceMin = arc.getDistance();
+                resultat = arc.getSommetB();
+            }
+        }
+
+        return resultat;
     }
 }
